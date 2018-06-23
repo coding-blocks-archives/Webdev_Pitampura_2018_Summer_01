1a23e08429dc410eb1732674a57411a266628e65
Arnav Gupta <arnav@codingblocks.com>
2018-06-23 17:00:11 +0530

    alert box with user input
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture06/js_01/script.js
+++ Lecture06/js_01/script.js
@@ -1,5 +1,16 @@
 console.log("HELLO")
 
+function showAlert(name) {
+    window.alert("Hello " + name)
+}
+
 window.onload = function () {
     document.getElementById('mydiv').innerText = "SOMETHING"
+
+    var alertbtn = document.getElementById('alertbtn')
+    var nameinput = document.getElementById('nameinput')
+
+    alertbtn.onclick = function() {
+        showAlert(nameinput.value)
+    }
 }
