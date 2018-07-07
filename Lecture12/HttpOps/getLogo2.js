9d458043b2b187f32df15b105a4d1b42ced2d842
Arnav Gupta <arnav@codingblocks.com>
2018-07-07 17:45:19 +0530

    get logo
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture12/HttpOps/getLogo2.js
+++ Lecture12/HttpOps/getLogo2.js
@@ -4,12 +4,19 @@ const fs = require('fs')
 https.get(
   'https://codingblocks.com/assets/images/cb/cblogo.png',
   (res) => {
+    res.setEncoding('binary')
+
+    let image = ''
 
     res.on('data', (chunk) => {
+      image += chunk
+    })
+
+    res.on('end', () => {
       fs.writeFile(
-        __dirname + '/logo.png',
-        chunk,
-        {flag: 'a'},
+        __dirname + '/logo2.png',
+        image,
+        'binary',
         (err) => console.error(err)
       )
     })
