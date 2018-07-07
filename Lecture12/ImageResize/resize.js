13e881c8689266ab5c61168569054e5416484d3a
Arnav Gupta <arnav@codingblocks.com>
2018-07-07 18:23:04 +0530

    resize image
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture12/ImageResize/resize.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const resize = require('resize-img')
+
+fs.readFile(
+  __dirname + '/cblogo.png',
+  (err, data) => {
+    if (err) throw err
+
+    // 'data' is the buffer of logo original png
+
+    resize(data, {width: 100, height: 50})
+      .then((buff) => {
+
+        // .then() runs after resizing asynchronously
+        // buff here is the buffer of modified image
+        fs.writeFile(
+          __dirname + '/smalllogo.png',
+          buff,
+          (err) => {
+            if (err) throw err
+
+            // Now resized file has been saved
+            console.log('DONE')
+          }
+        )
+      })
+  }
+)
