6bc0409c1d13d14ce78a533bdf8a6a4729a07a73
Arnav Gupta <arnav@codingblocks.com>
2018-07-17 17:03:09 +0530

    server rendered todolist
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture16/Handlebars/server.js
+++ Lecture16/Handlebars/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.set("view engine", "hbs")
+let todos = [
+  {task: 'first task'},
+  {task: 'second task'}
+]
+// let todos ["task one", "task two"]
 
 app.get('/', (req, res) => {
-  res.render('main', {body: 'Hello World'})
+  res.render('main', {
+    todos
+  })
 })
 
+app.post('/', (req, res) => {
+  todos.push({task: req.body.task})
+  res.redirect('/')
+})
 
 app.listen(5454, function () {
     console.log("Server started on http://localhost:5454");
