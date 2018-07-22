d61ba20cc6855fc9b42a6e169260d401675949b0
Arnav Gupta <arnav@codingblocks.com>
2018-07-22 16:48:33 +0530

    subjects
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture17/MarkSheet/controllers/subjects.js
+++ Lecture17/MarkSheet/controllers/subjects.js
@@ -1,5 +1,7 @@
 const {Subject}  = require('../db/models')
 
 module.exports = {
-  getAllSubjects: async () => await Subject.findAll()
+  getAllSubjects: async () => await Subject.findAll(),
+  addSubject: async(name, maxMarks) =>
+    await Subject.create({name, maxMarks}),
 }
--- Lecture17/MarkSheet/routes/api/subjects.js
+++ Lecture17/MarkSheet/routes/api/subjects.js
@@ -1,3 +1,22 @@
 const route = require('express').Router()
+const subjectController = require('../../controllers/subjects')
+/**
+ * GET /api/subjects
+ */
+route.get('/', async (req, res) => {
+  res.status(200).json(await subjectController.getAllSubjects())
+})
+
+/**
+ * POST /api/subjects
+ *    @param name
+ *    @param max_marks
+ */
+route.post('/', async (req, res) => {
+  res.status(201).json(await subjectController.addSubject(
+    req.body.name,
+    req.body.max_marks
+  ))
+})
 
 module.exports = route
